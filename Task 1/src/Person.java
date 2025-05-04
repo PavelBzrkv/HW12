3b6a557f9c84147c525fa03dccc30f4cda14b24f
PavelBzrkv <pavel95959@gmail.com>
2025-05-04 15:40:43 +0500

    Commit

---
+++ Task 1/src/Person.java
@@ -0,0 +1,58 @@
+public class Person {
+    private String name;
+    private String profession;
+    private String workPlace;
+    private int age;
+    private double salary;
+
+    public Person() {
+    }
+
+    public Person(String name, String profession, String workPlace, int age, double salary) {
+        this.name = name;
+        this.profession = profession;
+        this.workPlace = workPlace;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getWorkPlace() {
+        return workPlace;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
